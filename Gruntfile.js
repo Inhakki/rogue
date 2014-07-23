cb4ab33f30381dbe8d31cfd1aeb83eda9d3458bb
Mark Kennedy <mark.kennedy@akqa.com>
2014-07-23 12:16:34 -0400

    Making build process respect options passed to build-clean command

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,14 +3,20 @@
 }());
 
 module.exports = function(grunt) {
+
     require('load-grunt-config')(grunt, {
-
         init: true,
-
         loadGruntTasks: {
             pattern: ['grunt-*']
         }
     });
 
     grunt.loadNpmTasks('akqa-core');
+    
+    // add build-clean job
+    grunt.config('build-clean', {
+            all: {
+                src: ['build/**']
+            }
+    });
 };

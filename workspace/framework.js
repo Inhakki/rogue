c3e000c509a20c33076dd64c4e565dc7f83a025e
Mark Kennedy <mark.kennedy@akqa.com>
2014-07-22 21:47:48 -0400

    Added underscore dependency

--- workspace/framework.js
+++ workspace/framework.js
@@ -2,7 +2,8 @@ if (typeof frameworkConfig === 'undefined') {
     console.error('web framework load error: no frameworkConfig is specified');
 }
 
-define(function () {
+// must require underscore immediately to make available to framework modules
+define([frameworkConfig.modulePath + '/libs/underscore'], function () {
     return {
         config: frameworkConfig
     };

ee523c438d492c55f5a8f82e64c255686eabd12c
Mark Kennedy <mark.kennedy@akqa.com>
2014-07-25 08:30:35 -0400

    Created new test utils file with createEvent method to standardize event creation across test ran locally in browser and headlessly

---
+++ tests/test-utils.js
@@ -0,0 +1,33 @@
+define(function () {
+
+    var TestUtils = function () {};
+
+    TestUtils.prototype = {
+
+        /**
+         * Creates an event.
+         * @param {string} name - The event name
+         * @param {object} options - Options to be passed to event
+         */
+        createEvent: function (name, options) {
+            var event,
+                bubbles,
+                cancelable,
+                options = options || {};
+            if (typeof Event === 'function') {
+                event = new Event(name, options);
+            } else {
+                // must register click old-fashioned way so that running tests headlessly will work
+                event = document.createEvent('Event');
+                bubbles = options.bubbles || true;
+                cancelable = options.cancelable || true;
+                event.initEvent(name, bubbles, cancelable);
+            }
+            return event;
+        }
+
+    };
+
+    return new TestUtils();
+
+});

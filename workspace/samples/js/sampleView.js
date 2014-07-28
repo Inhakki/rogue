8e1e6832313e11a736433433935f6bc3a0ddb503
Mark Kennedy <mark.kennedy@akqa.com>
2014-07-28 17:29:43 -0400

    Added new checkbox class to module samples page to illustrate necessary markup + fixed samples page to load correctly

--- workspace/samples/js/sampleView.js
+++ workspace/samples/js/sampleView.js
@@ -1,13 +1,27 @@
+
+require.config({
+    'baseUrl': '',
+    'paths': {
+        framework: '../../'
+    }
+});
+
+
 require([
     'framework/framework',
     'framework/utils',
-    'framework/ui/tooltip'
-], function (App, Utils, Tooltip) {
+    'framework/ui/tooltip',
+    'framework/ui/checkbox'
+], function (App, Utils, Tooltip, Checkbox) {
     'use strict';
 
     var SampleView = function () {
         this.tooltip = new Tooltip({
             el: Utils.getElementsByClassName('ui-tooltip')[0]
+        });
+
+        this.checkbox = new Checkbox({
+            el: Utils.getElementsByClassName('ui-checkbox')[0]
         });
     };
 

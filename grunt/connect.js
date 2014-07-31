bdd3a0187d8443cab65d9cd5f32834753070ce69
Mark Kennedy <mark.kennedy@akqa.com>
2014-07-31 09:50:25 -0400

    Build cleanup + added new web-framework url to readme

--- grunt/connect.js
+++ grunt/connect.js
@@ -1,14 +1,6 @@
 module.exports = {
-    test: {
-        options: {
-            hostname: 'localhost',
-            port: 7000
-        }
-    },
     local: {
         options: {
-            hostname: 'localhost',
-            port: 9000,
             keepalive: true,
             options: { livereload: true }
         }

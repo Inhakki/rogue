3c223d3a54485d80d21c840252351328b5887e73
Mark Kennedy <mark.kennedy@akqa.com>
2014-07-31 21:31:50 -0400

    Adding make test connect server back in

--- grunt/connect.js
+++ grunt/connect.js
@@ -4,5 +4,11 @@ module.exports = {
             keepalive: true,
             options: { livereload: true }
         }
+    },
+    test: {
+        options: {
+            hostname: 'localhost',
+            port: 7000
+        }
     }
 };

e4053a1eb65ef66683a4273ce68f58bd727683f2
Mark Kennedy <mark.kennedy@akqa.com>
2014-08-15 10:04:02 -0400

    Moving modal.js

--- workspace/modules/modal.js
+++ workspace/modules/modal.js
@@ -13,24 +13,26 @@ define([
         /**
          * Sets up the modal.
          * @param {object} options - The options
-         * @param {HTMLElement} [options.el] - The element that should be used as the modal's container
-         * @param {HTMLElement} [options.contentEl] - The element that contains the modal content which gets nested inside the modal container
+         * @param {HTMLElement} options.containerEl - The element that should be used as the modal's container
+         * @param {HTMLElement} options.el - The element that contains the modal content which gets nested inside the modal container
          * @param {Function} [options.onHide] - A function that gets fired when the modal hides.
          * @param {Function} [options.onShow] - A function that gets fired when the modal shows.
          * @param {Function} [options.onClickOutside] - When tapping outside of the modal
+         * @param {string} [options.activeClass] - The class that gets added to the modal container when the modal is showing
          */
         initialize: function (options) {
 
             this.options = Utils.extend({
-                el: Utils.getElementsByClassName('modal')[0],
-                contentEl: null,
+                containerEl: document.getElementsByTagName('body')[0],
+                el: null,
                 onHide: null,
                 onShow: null,
                 onClickOutside: this.hide.bind(this),
                 activeClass: 'modal-active'
             }, options);
 
-            this.el = this.options.el;
+            this.container = this.options.containerEl;
+            this.content = this.options.el;
 
         },
 
@@ -38,10 +40,10 @@ define([
          * Shows the modal.
          */
         show: function () {
-            if (!this.el.contains(this.options.contentEl)) {
-                this.el.appendChild(this.options.contentEl);
+            if (!this.container.contains(this.content)) {
+                this.container.appendChild(this.content);
             }
-            Utils.addClass(this.el, this.options.activeClass);
+            Utils.addClass(this.container, this.options.activeClass);
             Utils.addEventListener(document, 'click', this._onDocClick.bind(this));
             if (this.options.onShow) {
                 this.options.onShow();
@@ -52,7 +54,7 @@ define([
          * Hides the modal.
          */
         hide: function () {
-            Utils.removeClass(this.el, this.options.activeClass);
+            Utils.removeClass(this.container, this.options.activeClass);
             Utils.removeEventListener(document, 'click', this._onDocClick.bind(this));
             if (this.options.onHide) {
                 this.options.onHide();
@@ -64,7 +66,7 @@ define([
          * @returns {boolean} Returns truthy if showing, falsy if not
          */
         isActive: function () {
-            return Utils.hasClass(this.el, this.options.activeClass);
+            return Utils.hasClass(this.container, this.options.activeClass);
         },
 
         /**
@@ -74,7 +76,7 @@ define([
          */
         _onDocClick: function (e) {
             var clickedItem = e.target,
-                isClickOutside = !this.options.contentEl.contains(clickedItem);
+                isClickOutside = !this.content.contains(clickedItem);
             if (isClickOutside && this.isActive()) {
                 if (this.options.onClickOutside) {
                     this.options.onClickOutside();
@@ -86,8 +88,8 @@ define([
          * Destroys the modal.
          */
         destroy: function () {
-            Utils.removeClass(this.el, this.options.activeClass);
-            this.el.removeChild(this.options.contentEl);
+            Utils.removeClass(this.container, this.options.activeClass);
+            this.container.removeChild(this.content);
             Utils.removeEventListener(document, 'click', this._onDocClick.bind(this));
         }
 

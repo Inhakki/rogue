711e54c604e640a528e4efc5e26f78e1dd4653d4
Mark Kennedy <mark.kennedy@akqa.com>
2014-08-23 08:02:38 -0400

    converted to CacheManager use local/session storage, added in callback functionality,  added expiration support, removed password handling from getting/setting

--- tests/cache-manager-tests.js
+++ tests/cache-manager-tests.js
@@ -11,39 +11,70 @@ define([
 
     QUnit.module('Cache Manager Tests');
 
-    QUnit.test('caching data', function () {
+    QUnit.asyncTest('setting and getting persistent data', function () {
         QUnit.expect(3);
         var dataKey = 'myKey';
         var mockData = {testing: 'data', it: 'should work'};
-        CacheManager.start();
-        QUnit.equal(CacheManager.getValue(dataKey), undefined, 'data is not cached initially');
-        CacheManager.setValue(dataKey, mockData);
-        QUnit.deepEqual(CacheManager.getValue(dataKey), mockData, 'caching data and then retrieving it was successful');
-        CacheManager.flushValue(dataKey);
-        QUnit.equal(CacheManager.getValue(dataKey), null, 'after flushing data, data is no longer available');
-        CacheManager.stop();
-        CacheManager.flushAll();
+        var localStorageGetStub = Sinon.stub(localStorage, 'getItem');
+        var localStorageSetStub = Sinon.stub(localStorage, 'setItem');
+        var localStorageRemoveStub = Sinon.stub(localStorage, 'removeItem');
+        CacheManager.setValue(dataKey, mockData, null, function () {
+            QUnit.deepEqual(localStorageSetStub.args[0], [dataKey, mockData], 'calling the setter method calls correct localStorage method with correct args');
+            CacheManager.getValue(dataKey, function () {
+                QUnit.deepEqual(localStorageGetStub.args[0], [dataKey], 'calling the getter method calls correct localStorage method with correct args');
+                CacheManager.flushValue(dataKey, function () {
+                    QUnit.deepEqual(localStorageRemoveStub.args[0], [dataKey], 'calling the remove method calls correct localStorage method with correct args when localStorage contains the value');
+                    localStorageGetStub.restore();
+                    localStorageSetStub.restore();
+                    localStorageRemoveStub.restore();
+                    QUnit.start();
+                });
+            });
+        });
     });
 
-    QUnit.test('caching secret, password-protected data ', function() {
-        QUnit.expect(7);
+    QUnit.asyncTest('setting and getting session data', function () {
+        QUnit.expect(3);
         var dataKey = 'myKey';
-        var secretKey = 's78d9AGEdj2493jklda10sdgjdfgj@';
         var mockData = {testing: 'data', it: 'should work'};
-        CacheManager.start();
-        QUnit.equal(CacheManager.getValue(dataKey), undefined, 'data is not cached initially');
-        CacheManager.setValue(dataKey, mockData, secretKey);
-        QUnit.equal(CacheManager.getValue(dataKey), undefined, 'trying to retrieve protected data without a secret key returns nothing');
-        QUnit.equal(CacheManager.getValue(dataKey, 'wrongpw'), undefined, 'trying to retrieve protected data with a wrong secret key returns nothing');
-        QUnit.deepEqual(CacheManager.getValue(dataKey, secretKey), mockData, 'trying to retrieve protected data with the correct secret key returns the object');
-        CacheManager.flushValue(dataKey);
-        QUnit.equal(CacheManager.getValue(dataKey, secretKey), mockData, 'trying to remove protected data without its secret key is not successful');
-        CacheManager.flushValue(dataKey, 'wrongpw');
-        QUnit.equal(CacheManager.getValue(dataKey, secretKey), mockData, 'trying to remove protected data with a wrong secret key is not successful');
-        CacheManager.flushValue(dataKey, secretKey);
-        QUnit.equal(CacheManager.getValue(dataKey), null, 'removing protected data with correct secret is successful');
-        CacheManager.stop();
-        CacheManager.flushAll();
+        var sessionStorageGetStub = Sinon.stub(sessionStorage, 'getItem');
+        var sessionStorageSetStub = Sinon.stub(sessionStorage, 'setItem');
+        var sessionStorageRemoveStub = Sinon.stub(sessionStorage, 'removeItem');
+        CacheManager.setSessionValue(dataKey, mockData, function () {
+            QUnit.deepEqual(sessionStorageSetStub.args[0], [dataKey, mockData], 'calling the setter method calls correct sessionStorage method with correct args');
+            CacheManager.getSessionValue(dataKey, function () {
+                QUnit.deepEqual(sessionStorageGetStub.args[0], [dataKey], 'calling the getter method calls correct sessionStorage method with correct args');
+                CacheManager.flushSessionValue(dataKey, function () {
+                    QUnit.deepEqual(sessionStorageRemoveStub.args[0], [dataKey], 'calling the remove method calls correct sessionStorage method with correct args when sessionStorage contains the value');
+                    sessionStorageGetStub.restore();
+                    sessionStorageSetStub.restore();
+                    sessionStorageRemoveStub.restore();
+                    QUnit.start();
+                });
+            });
+        });
+    });
+
+    QUnit.asyncTest('setting data with an expiration time', function () {
+        QUnit.expect(2);
+        var dataKey = 'myKey';
+        var mockData = {testing: 'data', it: 'should work'};
+        var localStorageGetStub = Sinon.stub(localStorage, 'getItem');
+        var localStorageSetStub = Sinon.stub(localStorage, 'setItem');
+        var localStorageRemoveStub = Sinon.stub(localStorage, 'removeItem');
+        var flushExpiration = 400;
+        var timed;
+        CacheManager.setValue(dataKey, mockData, flushExpiration, function () {
+            QUnit.equal(localStorageRemoveStub.callCount, 0, 'localStorage remove method was NOT called because timer did not run out yet');
+            timed = setTimeout(function () {
+                QUnit.deepEqual(localStorageRemoveStub.args[0], [dataKey], 'after timer runs out, localStorage method was called with correct args');
+                clearTimeout(timed);
+                localStorageGetStub.restore();
+                localStorageSetStub.restore();
+                localStorageRemoveStub.restore();
+                QUnit.start();
+            }, flushExpiration + 1);
+        });
     });
 
     QUnit.test('setting and getting cache data (using deprecated methods)', function() {
@@ -51,51 +82,18 @@ define([
         var dataKey = 'myKey';
         var secretKey = 's78d9AGEdj2493jklda10sdgjdfgj@';
         var mockData = {testing: 'data', it: 'should work'};
-        var getValueStub = Sinon.stub(CacheManager, 'getValue');
-        var setValueStub = Sinon.stub(CacheManager, 'setValue');
-        var flushValueStub = Sinon.stub(CacheManager, 'flushValue');
-        CacheManager.start();
+        var localStorageGetStub = Sinon.stub(localStorage, 'getItem');
+        var localStorageSetStub = Sinon.stub(localStorage, 'setItem');
+        var localStorageRemoveStub = Sinon.stub(localStorage, 'removeItem');
         CacheManager.cacheData(dataKey, mockData, secretKey);
-        QUnit.deepEqual(setValueStub.args[0], [dataKey, mockData, secretKey], 'calling deprecated cacheData() method calls setValue() with correct args');
+        QUnit.deepEqual(localStorageSetStub.args[0], [dataKey, mockData], 'calling deprecated cacheData() method calls setValue() with correct args');
         CacheManager.getCacheData(dataKey, secretKey);
-        QUnit.deepEqual(getValueStub.args[0], [dataKey, secretKey], 'calling deprecated getCacheData() method calls getValue() with correct args');
+        QUnit.deepEqual(localStorageGetStub.args[0], [dataKey], 'calling deprecated getCacheData() method calls getValue() with correct args');
         CacheManager.flushData(dataKey, secretKey);
-        QUnit.deepEqual(flushValueStub.args[0], [dataKey, secretKey], 'calling deprecated flushData() method calls flushValue() with correct args');
-        CacheManager.stop();
-        CacheManager.flushAll();
-        getValueStub.restore();
-        flushValueStub.restore();
-        setValueStub.restore();
+        QUnit.deepEqual(localStorageRemoveStub.args[0], [dataKey], 'calling deprecated flushData() method calls flushValue() with correct args');
+        localStorageGetStub.restore();
+        localStorageSetStub.restore();
+        localStorageRemoveStub.restore();
     });
-
-    QUnit.test('caching data when manager hasn\'t been started', function () {
-        QUnit.expect(5);
-        var dataKey = 'myKey';
-        var mockData = {testing: 'data', it: 'should work'};
-        var getValueSpy = Sinon.spy(CacheManager, 'getValue');
-        var setValueSpy = Sinon.spy(CacheManager, 'setValue');
-        try {
-            CacheManager.setValue();
-        } catch (e) {
-            QUnit.ok(setValueSpy.getCall(0).exception, 'an exception is thrown when trying to set data without starting cache manager');
-        }
-        try {
-            CacheManager.getValue();
-        } catch (e) {
-            QUnit.ok(getValueSpy.getCall(0).exception, 'an exception is thrown when trying to get data without starting cache manager');
-        }
-        CacheManager.start();
-        CacheManager.setValue(dataKey, mockData);
-        QUnit.ok(!setValueSpy.getCall(1).exception, 'no exception was thrown when caching data after manager has been started');
-        CacheManager.getValue(dataKey);
-        QUnit.ok(!getValueSpy.getCall(1).exception, 'no exception was thrown when retrieving data');
-        CacheManager.flushValue(dataKey);
-        QUnit.equal(CacheManager.getValue(dataKey), null, 'after flushing data, data is no longer available');
-        CacheManager.stop();
-        CacheManager.flushAll();
-        getValueSpy.restore();
-        setValueSpy.restore();
-    });
-
 
 });

37946dd9e8d1f7c253627224b711b98da5e00e95
Mark Kennedy <mark.kennedy@akqa.com>
2014-08-29 14:01:04 -0400

    Fixed issue in dispatching events via TestUtils to fix unit tests

--- tests/test-utils.js
+++ tests/test-utils.js
@@ -10,18 +10,18 @@ define(function () {
          * @param {object} options - Options to be passed to event
          */
         createEvent: function (name, options) {
-            var event,
-                bubbles,
-                cancelable,
-                options = options || {};
+            var event;
+
+            options = options || {};
+            options.bubbles = options.bubbles || false;
+            options.cancelable = options.cancelable|| false;
+
             if (typeof Event === 'function') {
                 event = new Event(name, options);
             } else {
                 // must register click old-fashioned way so that running tests headlessly will work
                 event = document.createEvent('Event');
-                bubbles = options.bubbles || false;
-                cancelable = options.cancelable || false;
-                event.initEvent(name, bubbles, cancelable);
+                event.initEvent(name, options.bubbles, options.cancelable);
             }
             return event;
         }

281fe7bc814ab0e5a9cb4b04dbef71ddb2a92cca
Mark Kennedy <mark.kennedy@akqa.com>
2014-12-22 10:19:35 -0500

    Started router files

---
+++ src/router.js
@@ -0,0 +1,19 @@
+define(function () {
+    "use strict";
+
+    /**
+     * A manager that handles routes throughout an app.
+     */
+    var Router = function () {};
+
+    Router.prototype = {
+
+        triggerRoute: function () {
+
+        }
+
+    };
+
+    return new Router();
+
+});
